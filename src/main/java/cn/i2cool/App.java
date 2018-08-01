5ab08bfbd260dbfd9121a882665b9c06212e7079
evis <evis.huang@1data.info>
2018-08-01 14:59:55 +0800

    init

---
+++ src/main/java/cn/i2cool/App.java
@@ -0,0 +1,13 @@
+package cn.i2cool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
